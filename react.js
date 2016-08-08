8b87aa915294bc886dd1a483bd273219a6eb4709
Kristoffer Brabrand <kristoffer@brabrand.no>
2016-08-08 13:41:14 +0200

    Another lame attempt

--- react.js
+++ react.js
@@ -2,6 +2,12 @@
 
 var extendConfig = require('./extend-config');
 
-module.exports = extendConfig(
-    require('eslint-config-airbnb')
-);
+module.exports = extendConfig({
+    extends: [
+        'eslint-config-airbnb-base',
+        'eslint-config-airbnb-base/rules/strict',
+        'eslint-config-airbnb/rules/react',
+        'eslint-config-airbnb/rules/react-a11y'
+    ].map(require.resolve),
+    rules: {}
+});
