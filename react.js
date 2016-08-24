d503d6d5af3f8b4859ea20674dae65d27f11d88d
Kristoffer Brabrand <kristoffer@brabrand.no>
2016-08-24 08:42:02 +0200

    Add missing colon in config

--- react.js
+++ react.js
@@ -11,6 +11,6 @@ module.exports = extendConfig({
     ].map(require.resolve),
     rules: {
         'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props' [2, 4]
+        'react/jsx-indent-props': [2, 4]
     }
 });
